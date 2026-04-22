200ab61ca1ad556fcba5d17caece488dc5490fce
CarlosJGarcia <cgarciams@hotmail.com>
2026-04-22 13:09:41 +0200

    06-01

--- 06/06-01.py
+++ 06/06-01.py
@@ -36,5 +36,22 @@ output = pipe(prompt)
 # Print result
 print(f"\nGenerated reply: {output[0]['generated_text']}")
 
+
+# Prompt components
+persona = "You are an expert in Large Language models. You excel at breaking down complex papers into digestible summaries.\n"
+instruction = "Summarize the key findings of the paper provided.\n"
+context = "Your summary should extract the most crucial points that can help researchers quickly understand the most vital information of the paper.\n"
+data_format = "Create a bullet-point summary that outlines the method. Follow this up with a concise paragraph that encapsulates the main results.\n"
+audience = "The summary is designed for busy researchers that quickly need to grasp the newest trends in Large Language Models.\n"
+tone = "The tone should be professional and clear.\n"
+text = "All pre-trained models are identified by a model ID. When you create a tokenizer that a pre-trained model requires, it will check with the pre-trained model’s config to instantiate the correct tokenizer object, similarly, for the model. Therefore, you just need to use AutoTokenizer and AutoModel instead of the specific classes, such as BertTokenizer and BertModel. Knowing how a transformer model usually works, you should expect the core model to take the input tokens and output logit tensors. Therefore, you used argmax above to convert the logits to token IDs and convert the IDs to strings using the tokenizer’s decode method. However, you must provide the access token if you want to use a gated model with the above code. The way to set up the access token is to use some environment variables. You can find all environment variables that matter to the transformers library in the documentation; the most important ones are:"
+data = f"Text to summarize: {text}"
+
+# The full prompt - remove and add pieces to view its impact on the generated output
+query = persona + instruction + context + data_format + audience + tone + data
+
+print(f"\n{query}")
+
+
 # 4. PREVENT EXIT: Wait for user signal
-input("Press ENTER to close the script and clear GPU memory")
+input(f"\nPress ENTER to close the script and clear GPU memory")
