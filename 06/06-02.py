44d248d101e1b57ec0a44374aedceaaed2011381
CarlosJGarcia <cgarciams@hotmail.com>
2026-04-23 06:50:06 +0200

    06-02

--- 06/06-02.py
+++ 06/06-02.py
@@ -1,10 +1,14 @@
+import warnings
+# Elimina un warning de librería huggingface_hub (Llama la usa para descargar el modelo desde Hugging Face Hub )
+warnings.filterwarnings("ignore", category=UserWarning, module="huggingface_hub")
+
+# Llama (mayúscula) es la clase y llama (minúscula) es el módulo (fichero)
 from llama_cpp.llama import Llama
 
-# Load Phi-3
-llm = Llama.from_pretrained(
-    repo_id="microsoft/Phi-3-mini-4k-instruct-gguf",
-    filename="*fp16.gguf",
-    n_gpu_layers=-1,
-    n_ctx=2048,
-    verbose=False
-)
+# Phi-3 version llama.cpp
+MODEL_ID = "microsoft/Phi-3-mini-4k-instruct-gguf"
+
+# Load Model
+llm = Llama.from_pretrained(repo_id=MODEL_ID, filename="*fp16.gguf", n_gpu_layers=-1, n_ctx=4096, verbose=False)
+
+print(f"\nEl modelo {MODEL_ID} se ha cargado y descargado correctamente.")
