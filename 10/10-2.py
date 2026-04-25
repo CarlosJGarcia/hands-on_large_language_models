9402b502145dc2e71fd2df479f25fd9aabc240a8
CarlosJGarcia <cgarciams@hotmail.com>
2026-04-25 11:21:04 +0200

    10-2

--- 10/10-2.py
+++ 10/10-2.py
@@ -1,5 +1,5 @@
-import mteb
-import warnings
+#import mteb
+#import warnings
 from datasets import load_dataset
 from sentence_transformers import SentenceTransformer
 from sentence_transformers.sentence_transformer import losses  
@@ -8,8 +8,7 @@ from sentence_transformers.sentence_transformer.training_args import SentenceTra
 from sentence_transformers.sentence_transformer.trainer import SentenceTransformerTrainer
 
 # Suppress FutureWarning from MTEB
-warnings.filterwarnings("ignore", category=FutureWarning, module="mteb")
-
+#warnings.filterwarnings("ignore", category=FutureWarning, module="mteb")
 
 # Load MNLI dataset from GLUE
 # 0 = entailment, 1 = neutral, 2 = contradiction
@@ -21,34 +20,49 @@ train_dataset = train_dataset.select_columns(['hypothesis', 'premise', 'label'])
 
 # Check one example
 print(f"\nTrain dataset successfully loaded.")
-print(f"Example:{train_dataset[2]}\n")
-
+print(f"Example: {train_dataset[2]}\n")
 
 # Use a base model
-MODEL_ID = "bert-base-uncased"
-embedding_model = SentenceTransformer(MODEL_ID)
-print(f"\nModel {MODEL_ID} successfully loaded.\n")
-
+#MODEL_ID = "bert-base-uncased"
+#embedding_model = SentenceTransformer(MODEL_ID)
+#print(f"\nModel {MODEL_ID} successfully loaded.\n")
+#
 # Define the loss function. In softmax loss, we will also need to explicitly set the number of labels.
-train_loss = losses.SoftmaxLoss(model=embedding_model, embedding_dimension=embedding_model.get_embedding_dimension(), num_labels=3)
+#train_loss = losses.SoftmaxLoss(model=embedding_model, embedding_dimension=embedding_model.get_embedding_dimension(), num_labels=3)
 
 # Create an embedding similarity evaluator for STSB
 val_sts = load_dataset("glue", "stsb", split="validation")
 evaluator = EmbeddingSimilarityEvaluator(sentences1=val_sts["sentence1"], sentences2=val_sts["sentence2"], scores=[score/5 for score in val_sts["label"]], main_similarity="cosine")
 print(f"Evaluator created.\n")
 
+# Load model
+MODEL_ID = "sentence-transformers/all-MiniLM-L6-v2"
+embedding_model = SentenceTransformer(MODEL_ID)
+print(f"\nModel {MODEL_ID} successfully loaded.\n")
+
+# Define the loss function
+train_loss = losses.MultipleNegativesRankingLoss(model=embedding_model)
+print(f"Loss function defined.\n")
+
 # Define the training arguments
-args = SentenceTransformerTrainingArguments(output_dir="base_embedding_model", num_train_epochs=1, per_device_train_batch_size=32, per_device_eval_batch_size=32, warmup_steps=100, fp16=True, eval_steps=100, logging_steps=100)
+#args = SentenceTransformerTrainingArguments(output_dir="base_embedding_model", num_train_epochs=1, per_device_train_batch_size=32, per_device_eval_batch_size=32, warmup_steps=100, fp16=True, eval_steps=100, logging_steps=100)
+args = SentenceTransformerTrainingArguments(output_dir="finetuned_embedding_model", num_train_epochs=1, per_device_train_batch_size=32, per_device_eval_batch_size=32, warmup_steps=100, fp16=True, eval_steps=100, logging_steps=100)
 
 # Train embedding model
+#trainer = SentenceTransformerTrainer(model=embedding_model, args=args, train_dataset=train_dataset, loss=train_loss, evaluator=evaluator)
+#trainer.train()
 trainer = SentenceTransformerTrainer(model=embedding_model, args=args, train_dataset=train_dataset, loss=train_loss, evaluator=evaluator)
+print(f"Training starts.\n")
 trainer.train()
+print(f"Training completed.\n")
+
 
 # Evaluate our trained model
+#evaluator(embedding_model)
 eval = evaluator(embedding_model)
-print(f"\nEvaluation: {eval}\n")
+print(f"Evaluation: {eval}\n")
 
 # Choose evaluation task
-tasks = mteb.get_tasks(tasks=["Banking77Classification"])
-eval2 = mteb.evaluate(model=embedding_model, tasks=tasks)
-print(f"Evaluation: {eval2}\n")
+#tasks = mteb.get_tasks(tasks=["Banking77Classification"])
+#eval2 = mteb.evaluate(model=embedding_model, tasks=tasks)
+#print(f"Evaluation: {eval2}\n")
