483fc93bd267eacb8a673bb813873de7fa1bc352
CarlosJGarcia <cgarciams@hotmail.com>
2026-04-28 13:33:33 +0200

    12-2

--- 12/12-2.py
+++ 12/12-2.py
@@ -1,4 +1,15 @@
+# Elimina un warning al usar LoRA adapter (alta precisión float16) con el modelo Tinyllama cuantizado 4-bit (NF4)
+# Idem al "apilar" dos modelos LoRA en la misma variable model 
+import warnings
+from trl import DPOConfig
+warnings.filterwarnings("ignore", message="Merge lora module to 4-bit linear")
+warnings.filterwarnings("ignore", message="Already found a .peft_config. attribute")
+
+from rich.console import Console
 from datasets import load_dataset
+from peft import AutoPeftModelForCausalLM
+from transformers import BitsAndBytesConfig, AutoTokenizer
+from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
 
 def format_prompt(example):
     """Format the prompt to using the <|user|> template TinyLLama is using"""
@@ -28,4 +39,63 @@ dpo_dataset = dpo_dataset.filter(
 dpo_dataset = dpo_dataset.map(
     format_prompt,  remove_columns=dpo_dataset.column_names
 )
-dpo_dataset
+
+console = Console()
+console.print(f"\nTrain dataset successfully loaded.", style="gold1")
+print(f"Structure: {dpo_dataset}\n")
+
+
+# 4-bit quantization configuration - Q in QLoRA
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,  # Use 4-bit precision model loading
+    bnb_4bit_quant_type="nf4",  # Quantization type
+    bnb_4bit_compute_dtype="float16",  # Compute dtype
+    bnb_4bit_use_double_quant=True,  # Apply nested quantization
+)
+
+# Merge LoRA and base model
+model = AutoPeftModelForCausalLM.from_pretrained(
+    "TinyLlama-1.1B-qlora",
+    low_cpu_mem_usage=True,
+    device_map="auto",
+    quantization_config=bnb_config,
+)
+merged_model = model.merge_and_unload()
+
+# Load LLaMA tokenizer
+model_name = "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = "<PAD>"
+tokenizer.padding_side = "left"
+
+# Prepare LoRA configuration
+peft_config = LoraConfig(
+    lora_alpha=32,  # LoRA Scaling
+    lora_dropout=0.1,  # Dropout for LoRA Layers
+    r=64,  # Rank
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=  # Layers to target
+     ["k_proj", "gate_proj", "v_proj", "up_proj", "q_proj", "o_proj", "down_proj"]
+)
+
+# prepare model for training
+model = prepare_model_for_kbit_training(model)
+model = get_peft_model(model, peft_config)
+
+output_dir = "./results-2"
+
+# Training arguments
+training_arguments = DPOConfig(
+    output_dir=output_dir,
+    per_device_train_batch_size=2,
+    gradient_accumulation_steps=4,
+    optim="paged_adamw_32bit",
+    learning_rate=1e-5,
+    lr_scheduler_type="cosine",
+    max_steps=200,
+    logging_steps=10,
+    fp16=True,
+    gradient_checkpointing=True,
+    warmup_steps=0.1
+)
