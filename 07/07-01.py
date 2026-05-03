d7c24b0a3008924d1bf91093919d061bf2ce5f0b
CarlosJGarcia <cgarciams@hotmail.com>
2026-05-03 08:22:24 +0200

    07

--- 07/07-01.py
+++ 07/07-01.py
@@ -6,16 +6,16 @@ MODEL_PATH = "../models/Phi-3-mini-4k-instruct-fp16.gguf"
 
 # Cargando el modelo
 print(f"Loading model {MODEL_PATH} in the GPU")
-llm = LlamaCpp(model_path=MODEL_PATH, n_gpu_layers=-1, max_tokens=1024, n_ctx=4096, seed=42, verbose=False)
+model = LlamaCpp(model_path=MODEL_PATH, n_gpu_layers=-1, max_tokens=1024, n_ctx=4096, seed=42, verbose=False)
 
-# Local, GPU-accelerated LLM inference
+# Local, GPU-accelerated LLM inference. Uso el método invoke() del objeto llm que devuelve LlamaCpp al cargar el LLM
 question = "What are the advantages of using 16-bit precision in AI?"
 prompt = "<|user|>\n" + question + "<|end|>\n<|assistant|>\n"
 console = Console()
 console.print(f"\n--- Sending Prompt ---", style="gold1")
 print(prompt)
     
-response = llm.invoke(prompt)
+response = model.invoke(prompt)
 console.print(f"\n--- Response ---", style="gold1")
 print(response)
 
@@ -24,7 +24,7 @@ print()
 key = input("Press ENTER to exit.")
 
 # Manual cleanup to prevernt an Exception ignored in: <function Llama.__del__ at 0x7f99fb00a700> error when exiting the python script
-del llm
+del model
 print()
 
 
--- version.py
+++ version.py
@@ -10,19 +10,22 @@ import transformers
 
 # llama.cpp
 import llama_cpp
+import langchain_community
 from llama_cpp.llama import Llama
+
 
 print()
 print("Apple Silicon acceleration:", torch.backends.mps.is_available())
 print("CUDA enabled              :", torch.cuda.is_available())
 if torch.cuda.is_available():
     print("CUDA Compute Platform:", torch.version.cuda)
-print(f"Python version  : {sys.version_info.major}.{sys.version_info.minor}")
-print(f"PyTorch         : {torch.__version__}")
-print(f"Tiktoken        : {version('tiktoken')}")
-print(f"Tokenizers      : {tokenizers.__version__}")
-print(f"Bitsandbytes    : {version('bitsandbytes')}")
-print(f"llama-cpp-python: {llama_cpp.__version__}")
-print(f"Transformers    : {transformers.__version__}")
-print(f"OpenAI          : {openai.__version__}")
+print(f"Python version     : {sys.version_info.major}.{sys.version_info.minor}")
+print(f"PyTorch            : {torch.__version__}")
+print(f"Tiktoken           : {version('tiktoken')}")
+print(f"Tokenizers         : {tokenizers.__version__}")
+print(f"Bitsandbytes       : {version('bitsandbytes')}")
+print(f"llama-cpp-python   : {llama_cpp.__version__}")
+print(f"langchain-community: {langchain_community.__version__}")
+print(f"Transformers       : {transformers.__version__}")
+print(f"OpenAI             : {openai.__version__}")
 print()
